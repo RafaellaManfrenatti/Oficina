9284dbb54d5b427f93c5f80fbf1988386559e506
Rafaella Manfrenatti <112034945+RafaellaManfrenatti@users.noreply.github.com>
2022-11-23 21:40:46 -0300

    Add files via upload

---
+++ Mulheresnoite1-main/nossaoficina.php
@@ -0,0 +1,36 @@
+<?php
+require_once 'head.php';
+ require_once 'menu.php';
+?>
+
+<div class="container-fluid">
+    <div class="row text-center">
+    <div class="col-md-6"><img class="d-block w-100" src="carro1.webp" alt="imagemquemsomos">
+    <div class="col-md-6"> <h1> Nossa história</h1>
+             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium blanditiis, nemo autem reiciendis 
+        necessitatibus temporibus odit incidunt recusandae
+         mollitia dolore c</p></div>
+</div>
+    
+
+
+    <div class="row text-center">
+    
+    
+           
+    </div>
+
+   
+  
+</div>
+
+<div class="container-fluid">
+    <div class="col-md-12">
+        <h1>Nossos valores</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae 
+            quae beatae voluptas iusto nostrum doloribus, animi nam dolorum excepturi distinctio fugit 
+             perspiciatis illum rerum deserunt optio voluptate eaque minima!</p></div></div>
+
+             <?php
+             require_once 'footer.php';
+             ?>
